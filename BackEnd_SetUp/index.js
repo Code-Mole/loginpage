acc627aa4a701ab4ae83786a17698858e8a7639c
Asangabia Mole <moleasangabia23@gmail.com>
2024-06-07 22:48:37 +0000

    setting the backend

--- BackEnd_SetUp/index.js
+++ BackEnd_SetUp/index.js
@@ -4,19 +4,13 @@ import userRouter from "./Routes/userRouter.js";
 import productsRouter from "./Routes/productsRouter.js";
 import cors from "cors";
 
-
-
 const server = express();
 server.use(express.json());
 server.use(cors());
-server.use('/api/users',userRouter);
-server.use('/api/products',productsRouter);
-
-
-
-
+server.use("/api/users", userRouter);
+server.use("/api/products", productsRouter);
 
 const Port = 5030;
-server.listen(Port,()=>{
-    console.log(`Server is running on port ${Port}`)
-})
+server.listen(Port, () => {
+  console.log(`Server is running on port ${Port}`);
+});
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,23 +1,21 @@
- 
- 
- 'use client'
-import React, { useState } from 'react'
-import styles from './login.module.css'
-import  axios from 'axios';
+"use client";
+import React, { useState } from "react";
+import styles from "./login.module.css";
+import axios from "axios";
 
 const registerUser = async (data) => {
-  await axios.post("http://localhost:5030/api/users/post",data)
-}
+  await axios.post("http://localhost:5030/api/users/post", data);
+};
 
-function login() {
-  const [errMsg , seterrMsg]=useState();
-  const [username, setusername] = useState('');
-  const [password, setpassword] = useState('');
+function Login() {
+  const [errMsg, seterrMsg] = useState();
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
 
   const data = {
-    username,password
-  }
-
+    username,
+    password,
+  };
 
   //  const loginbtn = (e)=>{
   //   // e.preventDefault();
@@ -43,20 +41,49 @@ function login() {
   //  }
 
   return (
-    <div className={styles.loginContainer}> 
+    <div className={styles.loginContainer}>
       <h1 className={styles.loginTitle}>Admin Login</h1>
       <div className={styles.formContainerBig}>
         <form className={styles.formContainer} action="">
-          <input onChange={(e)=>(setusername(e.target.value))} id='name' className={styles.inputStyles} type="text" name='userName' placeholder='Username ' />
-          <input onChange={(e)=>(setpassword(e.target.value))} id='password' className={styles.inputStyles} type="password"  name='password' placeholder='Password ' />
+          <input
+            onChange={(e) => setusername(e.target.value)}
+            id="name"
+            className={styles.inputStyles}
+            type="text"
+            name="userName"
+            placeholder="Username "
+          />
+          <input
+            onChange={(e) => setpassword(e.target.value)}
+            id="password"
+            className={styles.inputStyles}
+            type="password"
+            name="password"
+            placeholder="Password "
+          />
           <span id="msg" className={styles.msgErr}></span>
-           <a id={styles.lossPassword} href="/">Lost Password?</a>
-          <button  id='btn' className={styles.buttonStyles} type="submit" onClick={registerUser()}>Submit</button>
+          <a id={styles.lossPassword} href="/">
+            Lost Password?
+          </a>
+          <button
+            id="btn"
+            className={styles.buttonStyles}
+            type="submit"
+            onClick={registerUser()}
+          >
+            Submit
+          </button>
         </form>
       </div>
-      <p id={styles.para}> I don`t have an account <a href="signup" id={styles.createStyle}>Create Account</a></p>
+      <p id={styles.para}>
+        {" "}
+        I don`t have an account{" "}
+        <a href="signup" id={styles.createStyle}>
+          Create Account
+        </a>
+      </p>
     </div>
-  )
+  );
 }
 
-export default login
+export default Login;
